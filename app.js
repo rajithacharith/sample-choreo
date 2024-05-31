1fc87acf306bbc82144dbb0e3f8bb90340c78604
rajithacharith <rajithacharith@gmail.com>
2024-05-31 09:49:03 +0530

    log headers

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // log headers
+  console.log(req.headers);
+  res.send(res.headers)
 })
 
 app.listen(port, () => {
